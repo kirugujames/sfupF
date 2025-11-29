57593f63d8a684be95b45387a8d57e07a20a3605
jameskk-syy <ronaldoeden70@gmail.com>
2025-11-29 23:24:16 +0300

    pulling nico

--- app/admin/ui/blogs/page.tsx
+++ app/admin/ui/blogs/page.tsx
@@ -1,7 +1,40 @@
-const page = () => {
-  return (
-    <div>page</div>
-  )
-};
+import { AppSidebar, DocumentItem, NavItem } from "@/components/app-sidebar"
+import { ChartAreaInteractive } from "@/components/chart-area-interactive"
+import { DataTable } from "@/components/data-table"
+import { SectionCards } from "@/components/section-cards"
+import { SiteHeader } from "@/components/site-header"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import { documents, navMain, navSecondary, user } from "../../dashboard/nav-items"
 
-export default page;
+export default function Page() {
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar
+        variant="inset"
+        navItems={navMain}
+        documents={documents}
+        user={user}
+        navSecondary={navSecondary}
+      />
+      <SidebarInset>
+        <SiteHeader title="Dashboard" />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <div className="px-4 lg:px-6">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
--- components/register-form.tsx
+++ components/register-form.tsx
@@ -4,18 +4,66 @@ import type React from "react"
 
 import { useState } from "react"
 import { CheckCircle } from "lucide-react"
+import api from "@/lib/axios"
+import toast, { Toaster } from "react-hot-toast"
+import { Spinner } from "./ui/spinner"
 
 export function RegisterForm() {
   const [submitted, setSubmitted] = useState(false)
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [dob, setDob] = useState("");
+  const [gender, setGender] = useState("");
+  const [phone, setPhone] = useState("");
+  const [idNo, setIdNo] = useState("");
+  const [doc_type, setDocType] = useState("");
+  const [Constituency, setConstituency] = useState("");
+  const [ward, setWard] = useState("");
+  const [county, setCounty] = useState("");
+  const [area_of_interest, setAreaOfInterest] = useState("");
+  const [username, setUsername] = useState("");
+  const [role_id, setRoleId] = useState(2);
+const resetForm = () => {
+  setFirstName("");
+  setLastName("");
+  setEmail("");
+  setDob("");
+  setGender("");
+  setPhone("");
+  setIdNo("");
+  setDocType("");
+  setConstituency("");
+  setWard("");
+  setCounty("");
+  setAreaOfInterest("");
+};
 
-  const handleSubmit = (e: React.FormEvent) => {
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    setSubmitted(true);
     e.preventDefault()
-    setSubmitted(true)
-    setTimeout(() => setSubmitted(false), 3000)
+    try {
+      setSubmitted(false);
+      const response = await api.post("api/members/register/member", {
+        first_name, last_name, email, dob, gender, phone, idNo, doc_type, Constituency, ward, county, area_of_interest, role_id, username
+      })
+      if (response.data?.statusCode == 201) {
+        resetForm();
+        toast.success(response.data?.message);
+      } else {
+        toast.error(response.data?.message);
+
+      }
+    } catch (error) {
+      setSubmitted(false);
+      toast.success("something wrong happened");
+    }
   }
 
   return (
     <section className="w-full py-16 md:py-24 bg-background">
+      <Toaster position="top-right" />
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex md:flex-row flex-col  gap-12 items-start">
           {/* Left Column - Info */}
@@ -78,6 +126,8 @@ export function RegisterForm() {
                   <input
                     type="text"
                     required
+                    value={first_name}
+                    onChange={(e) => setFirstName(e.target.value)}
                     className="w-full bg-white px-4 py-2 border border-border rounded-lg focus:outline-none focus:border-secondary"
                     placeholder="John"
                   />
@@ -87,6 +137,8 @@ export function RegisterForm() {
                   <input
                     type="text"
                     required
+                    value={last_name}
+                    onChange={(e) => setLastName(e.target.value)}
                     className="w-full bg-white px-4 py-2 border border-border rounded-lg focus:outline-none focus:border-secondary"
                     placeholder="Doe"
                   />
@@ -101,6 +153,8 @@ export function RegisterForm() {
                   <input
                     type="email"
                     required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     className="w-full bg-white px-4 py-2 border border-border rounded-lg focus:outline-none focus:border-secondary"
                     placeholder="john@example.com"
                   />
@@ -111,6 +165,8 @@ export function RegisterForm() {
                   <input
                     type="tel"
                     required
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
                     className="w-full bg-white px-4 py-2 border border-border rounded-lg focus:outline-none focus:border-secondary"
                     placeholder="+254 712 345 678"
                   />
@@ -123,6 +179,8 @@ export function RegisterForm() {
                   <input
                     type="date"
                     required
+                    value={dob}
+                    onChange={(e) => setDob(e.target.value)}
                     className="w-full bg-white px-4 py-2 border border-border rounded-lg focus:outline-none focus:border-secondary"
                     placeholder="+254 712 345 678"
                   />
@@ -132,6 +190,8 @@ export function RegisterForm() {
                   <label className="block text-sm font-medium text-gray-600 mb-2">Sex *</label>
                   <select
                     required
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value)}
                     className="w-full bg-white px-4 py-2 border border-border rounded-lg focus:outline-none focus:border-secondary"
                   >
                     <option className="text-gray-400" value="">Select Gender</option>
@@ -147,6 +207,8 @@ export function RegisterForm() {
                   <label className="block text-sm font-medium text-gray-600 mb-2">Document Type *</label>
                   <select
                     required
+                    value={doc_type}
+                    onChange={(e) => setDocType(e.target.value)}
                     className="w-full bg-white px-4 py-2 border border-border rounded-lg focus:outline-none focus:border-secondary"
                   >
                     <option className="text-gray-400" value="">Select Document Type</option>
@@ -160,6 +222,8 @@ export function RegisterForm() {
                   <input
                     type="number"
                     required
+                    value={idNo}
+                    onChange={(e) => setIdNo(e.target.value)}
                     className="w-full px-4 bg-white py-2 border border-border rounded-lg focus:outline-none focus:border-secondary"
                     placeholder="01234567"
                   />
@@ -172,6 +236,8 @@ export function RegisterForm() {
                   <label className="block text-sm font-medium text-gray-600 mb-2">County *</label>
                   <select
                     required
+                    value={county}
+                    onChange={(e) => setCounty(e.target.value)}
                     className="w-full bg-white px-4 py-2 border border-border rounded-lg focus:outline-none focus:border-secondary"
                   >
                     <option className="text-gray-400" value="">Select County</option>
@@ -187,6 +253,8 @@ export function RegisterForm() {
                   <label className="block text-sm font-medium text-gray-600 mb-2">Consituency *</label>
                   <select
                     required
+                    value={Constituency}
+                    onChange={(e) => setConstituency(e.target.value)}
                     className="w-full bg-white px-4 py-2 border border-border rounded-lg focus:outline-none focus:border-secondary"
                   >
                     <option className="text-gray-400" value="">Select Consituency</option>
@@ -205,6 +273,8 @@ export function RegisterForm() {
                   <label className="block text-sm font-medium text-gray-600 mb-2">Ward *</label>
                   <select
                     required
+                    value={ward}
+                    onChange={(e) => setWard(e.target.value)}
                     className="w-full bg-white px-4 py-2 border border-border rounded-lg focus:outline-none focus:border-secondary"
                   >
                     <option className="text-gray-400" value="">Select Ward</option>
@@ -220,6 +290,8 @@ export function RegisterForm() {
                   <label className="block text-sm font-medium text-gray-600 mb-2">Area of Interest *</label>
                   <select
                     required
+                    value={area_of_interest}
+                    onChange={(e) => setAreaOfInterest(e.target.value)}
                     className="w-full bg-white px-4 py-2 border border-border rounded-lg focus:outline-none focus:border-secondary"
                   >
                     <option className="text-gray-400" value="">Select Consituency</option>
@@ -230,17 +302,13 @@ export function RegisterForm() {
                   </select>
                 </div>
               </div>
-
-
-
-
-
               {/* Submit Button */}
               <button
                 type="submit"
+                disabled ={submitted}
                 className="w-full bg-secondary text-white py-3 rounded-lg font-bold hover:bg-secondary/90 transition-colors mt-6"
               >
-                Complete Registration
+                {submitted ? <Spinner /> : "Complete Registration"}
               </button>
 
               <p className="text-xs text-foreground/60 text-center mt-4">
